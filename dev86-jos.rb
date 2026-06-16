fd1b4cc17f79f7b7a9700b23c3cb2e901841c773
dyx <1437418067@qq.com>
2026-06-17 01:26:41 +0800

    dev86-jos.rb

--- dev86-jos.rb
+++ dev86-jos.rb
@@ -3,7 +3,7 @@ class Dev86Jos < Formula
   homepage "v3.sk/~lkundrak/dev86/"
   license "GPL-2.0"
   # 使用 Git 仓库，指定分支（默认 main/master）
-  head "https://github.com/elks-86/elks.git", branch: "master"
+  head "https://github.com/lkundrak/dev86.git", branch: "master"
 
   def install
     system "make"
