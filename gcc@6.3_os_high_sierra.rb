3823644c07076a5b637fd25f8ac957649e5f8e19
dengyouxu <1437418067@qq.com>
2026-07-05 12:49:21 +0800

    gcc@6.3_os_high_sierra.rb

--- gcc@6.3_os_high_sierra.rb
+++ gcc@6.3_os_high_sierra.rb
@@ -58,7 +58,7 @@ class GccAT63OsHighSierra < Formula
 
   # Use libraries from Homebrew or macOS
   patch do
-    url "https://raw.githubusercontent.com/DYX884877791/homebrew-i386-jos-elf-gcc/refs/heads/master/patches/gcc@6.3_os_high_sierra_fix1.patch"
+    url "https://raw.githubusercontent.com/DYX884877791/homebrew-i386-jos-elf-gcc/refs/heads/master/patches/gcc6.3/gcc@6.3_os_high_sierra_fix1.patch"
   end
 
   def install
