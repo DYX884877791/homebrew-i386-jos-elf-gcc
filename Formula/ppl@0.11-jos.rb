fe4c674cf2056487d713da65940330efa7d94529
dyx <1437418067@qq.com>
2026-07-09 08:55:45 +0800

    ppl@0.11-jos.rb

--- Formula/ppl@0.11-jos.rb
+++ Formula/ppl@0.11-jos.rb
@@ -18,9 +18,6 @@ class PplAT011Jos < Formula
 
   patch :DATA
 
-  # 第二个 patch
-  patch :DATA
-
   # https://www.cs.unipr.it/mantis/view.php?id=596
   # https://github.com/Homebrew/homebrew/issues/27431
   # Using different patch from upstream bug report to avoid autoreconf.
@@ -60,6 +57,7 @@ class PplAT011Jos < Formula
 end
 
 __END__
+diff -git ppl-0.11.orig/src/OR_Matrix.inlines.hh ppl-0.11/src/OR_Matrix.inlines.hh
 --- ppl-0.11.orig/src/OR_Matrix.inlines.hh	2026-07-09 10:00:00.000000000 +0800
 +++ ppl-0.11/src/OR_Matrix.inlines.hh	    2026-07-09 10:00:00.000000000 +0800
 @@ -97,9 +97,9 @@
@@ -75,18 +73,3 @@ __END__
    size_ = y.size_;
  #endif
 
-__END__
---- ppl-0.11.orig/src/Determinate.inlines.hh	2026-07-09 10:00:00.000000000 +0800
-+++ ppl-0.11/src/Determinate.inlines.hh	        2026-07-09 10:00:00.000000000 +0800
-@@ -285,10 +285,10 @@
-
- template <typename PSET>
- template <typename Binary_Operator_Assign>
- inline
--Determinate<PSET>::Binary_Operator_Assign_Lifter<Binary_Operator_Assign>
-+typename Determinate<PSET>::template Binary_Operator_Assign_Lifter<Binary_Operator_Assign>
- Determinate<PSET>::lift_op_assign(Binary_Operator_Assign op_assign) {
-   return Binary_Operator_Assign_Lifter<Binary_Operator_Assign>(op_assign);
- }
-
- } // namespace Parma_Polyhedra_Library
