38821b74c92efb0cea6f11c04e5c8aab92c9422d
dyx <1437418067@qq.com>
2026-07-09 13:29:38 +0800

    gcc@4.3.rb

--- Formula/gcc@4.3.rb
+++ Formula/gcc@4.3.rb
@@ -23,10 +23,10 @@ class GccAT43 < Formula
 
   # Fixes build with Xcode 7.
   # https://gcc.gnu.org/bugzilla/show_bug.cgi?id=66523
-  patch do
-    url "https://gcc.gnu.org/bugzilla/attachment.cgi?id=35773"
-    sha256 "db4966ade190fff4ed39976be8d13e84839098711713eff1d08920d37a58f5ec"
-  end
+  # patch do
+  #   url "https://gcc.gnu.org/bugzilla/attachment.cgi?id=35773"
+  #   sha256 "db4966ade190fff4ed39976be8d13e84839098711713eff1d08920d37a58f5ec"
+  # end
 
   depends_on "gmp@4-jos"
   depends_on "libmpc@0.8-jos"
