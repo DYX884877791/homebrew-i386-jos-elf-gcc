f17612705cae9d9f24b05ec882b8e4672aee9514
dyx <1437418067@qq.com>
2026-07-09 15:25:27 +0800

    gcc@4.3.rb

--- Formula/gcc@4.3.rb
+++ Formula/gcc@4.3.rb
@@ -122,7 +122,7 @@ class GccAT43 < Formula
       system "../configure", *args
 
       # Flags for Clang compatibility
-      make_flags = 'BOOT_CFLAGS="$BOOT_CFLAGS -D_FORTIFY_SOURCE=0" STAGE1_CFLAGS="$STAGE1_CFLAGS -std=gnu89 -D_FORTIFY_SOURCE=0 -fkeep-inline-functions"'
+      make_flags = 'BOOT_CFLAGS="-D_FORTIFY_SOURCE=0" STAGE1_CFLAGS="-std=gnu89 -D_FORTIFY_SOURCE=0 -fkeep-inline-functions"'
 
       if build.with? "profiled-build"
         # Takes longer to build, may bug out. Provided for those who want to
