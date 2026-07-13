2e91f4eb0c181453f35be9fae2c93c999ef03b3f
dyx <1437418067@qq.com>
2026-07-13 19:25:40 +0800

    crosstool-ng@1.21.0-jos.rb

---
+++ Formula/crosstool-ng@1.21.0-jos.rb
@@ -0,0 +1,56 @@
+class CrosstoolNgAT1210Jos < Formula
+  desc "Tool for building toolchains"
+  homepage "http://crosstool-ng.org"
+  url "http://crosstool-ng.org/download/crosstool-ng/crosstool-ng-1.21.0.tar.bz2"
+  sha256 "67122ba42657da258f23de4a639bc49c6ca7fe2173b5efba60ce729c6cce7a41"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "coreutils-jos" => :build
+  depends_on "wget"
+  depends_on "gnu-sed"
+  depends_on "gawk"
+  depends_on "binutils-jos"
+  depends_on "libelf"
+  depends_on "grep" => :optional
+  depends_on "make" => :optional
+
+  # Avoid superenv to prevent https://github.com/mxcl/homebrew/pull/10552#issuecomment-9736248
+  env :std
+
+  def install
+    args = ["--prefix=#{prefix}",
+            "--exec-prefix=#{prefix}",
+            "--with-objcopy=gobjcopy",
+            "--with-objdump=gobjdump",
+            "--with-readelf=greadelf",
+            "--with-libtool=glibtool",
+            "--with-libtoolize=glibtoolize",
+            "--with-install=ginstall",
+            "--with-sed=gsed",
+            "--with-awk=gawk",
+    ]
+
+    args << "--with-grep=ggrep" if build.with? "grep"
+
+    args << "--with-make=#{Formula["make"].opt_bin}/gmake" if build.with? "make"
+
+    args << "CFLAGS=-std=gnu89"
+
+    system "./configure", *args
+
+    # Must be done in two steps
+    system "make"
+    system "make", "install"
+  end
+
+  def caveats; <<-EOS.undent
+    You will need to install modern gcc compiler in order to use this tool.
+    EOS
+  end
+
+  test do
+    system "#{bin}/ct-ng", "version"
+  end
+end
