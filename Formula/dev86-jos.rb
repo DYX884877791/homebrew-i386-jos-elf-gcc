116a762feead469ffd6cb89ec9e7df7ff7964fa4
dyx <1437418067@qq.com>
2026-07-27 18:48:09 +0800

    dev86-jos.rb

--- Formula/dev86-jos.rb
+++ Formula/dev86-jos.rb
@@ -4,6 +4,8 @@ class Dev86Jos < Formula
   license "GPL-2.0"
   # 使用 Git 仓库，指定分支（默认 main/master）
   head "https://github.com/lkundrak/dev86.git", branch: "master"
+  url "https://github.com/DYX884877791/homebrew-i386-jos-elf-gcc/blob/master/Tarballsdev86-0.16.21.tar.gz"
+  sha256 "49c7ee9102d45c2eef072bfb8a49ba3821c9fe3ef31ccd3290f658b667b579a8"
 
   depends_on "gcc@4.6" => :build
 
